8d867851b97ee97f50ba5e1c8b11f2e389231f36
papercrane1001 <papercrane1001@gmail.com>
2024-03-20 10:10:48 -0700

    Initial commit

---
+++ 1.4/Source/PoweredObelisk/PoweredObelisk/CompProperties_PowPsyObelisk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using PsyObelisk;
+using Verse;
+
+namespace PoweredObelisk
+{
+    public class CompProperties_PowPsyObelisk : CompProperties
+    {
+        public CompProperties_PowPsyObelisk()
+        {
+            this.compClass = typeof(ThingComp_PowPsyObelisk)
+        }
+    }
+}
---
+++ 1.4/Source/PoweredObelisk/PoweredObelisk/ThingComp_PowPsyObelisk.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+//using PsyObelisk;
+using Verse;
+
+namespace PoweredObelisk
+{
+    public class ThingComp_PowPsyObelisk : ThingComp
+    {
+        public float focus;
+        public bool powerActive = true;
+        public bool getFocusActive = true;
+        public int lastPoweredTick;
+
+        public CompProperties_PowPsyObelisk Props
+        {
+            get
+            {
+                return props as CompProperties_PowPsyObelisk;
+            }
+        }
+        public float FocusMax
+        {
+            get
+            {
+                return (float)Utilities.Settings.psyFocusMax
+            }
+        }
+
+    }
+}
